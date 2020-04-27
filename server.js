648a4815ceee30160a0da921aa4d4f61808e0880
annastepanova <annastepanova1811@gmail.com>
2020-04-26 22:28:37 -0400

    hadling errors

--- server.js
+++ server.js
@@ -7,4 +7,12 @@ const app = express()
 
 app.use('/api/places', placesRoutes)
 
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error)
+  }
+  res.status(error.code || 500)
+  res.json({message: error.message || 'Unknown error occurred'})
+})
+
 app.listen(5000, () => { console.log("API listening on port 5000")})
