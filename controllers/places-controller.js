8a26efe6ef647b6a87b293e2b99a8aeb06076082
annastepanova <annastepanova1811@gmail.com>
2020-05-04 15:18:16 -0400

    added places controller

---
+++ controllers/places-controller.js
@@ -0,0 +1,47 @@
+const HttpError = require('../models/http-error')
+
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Boston Common',
+    description: 'Central public park in downtown Boston, Massachusetts',
+    address: '139 Tremont St, Boston, MA 02111',
+    location: {
+      lat: 42.355437,
+      lng: -71.0662329
+    },
+    creator: 'u1'
+  }
+] 
+
+const getPlaceById = (req, res, next) => {
+  const placeId = req.params.pid
+  const place = DUMMY_PLACES.find(p => {
+    return p.id === placeId
+  })
+
+  if (!place) {
+    throw new HttpError("Can't find place for the provided id", 404)
+  }
+
+  res.json({ place })
+
+}
+
+const getPlaceByUserId = (req, res, next) => {
+  const userId = req.params.uid
+  const place = DUMMY_PLACES.find(p => {
+    return p.creator === userId
+  })
+
+  if (!place) {
+    return next(
+      new HttpError("Can't find place for the provided user id", 404)
+    )
+  }
+
+  res.json({ place })
+}
+
+exports.getPlaceById = getPlaceById
+exports.getPlaceByUserId = getPlaceByUserId
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,50 +1,13 @@
 const express = require('express')
 
-const HttpError = require('../models/http-error')
+const placesControllers = require('../controllers/places-controller')
 
 const router = express.Router()
 
-const DUMMY_PLACES = [
-  {
-    id: 'p1',
-    title: 'Boston Common',
-    description: 'Central public park in downtown Boston, Massachusetts',
-    address: '139 Tremont St, Boston, MA 02111',
-    location: {
-      lat: 42.355437,
-      lng: -71.0662329
-    },
-    creator: 'u1'
-  }
-] 
 
-router.get('/:pid', (req, res, next) => {
-  const placeId = req.params.pid
-  const place = DUMMY_PLACES.find(p => {
-    return p.id === placeId
-  })
 
-  if (!place) {
-    throw new HttpError("Can't find place for the provided id", 404)
-  }
+router.get('/:pid', placesControllers.getPlaceById )
 
-  res.json({place})
-
-})
-
-router.get('/user/:uid', (req, res, next) => {
-  const userId = req.params.uid
-  const place = DUMMY_PLACES.find(p => {
-    return p.creator === userId
-  })
-
-  if (!place) {
-    return next(
-      new HttpError("Can't find place for the provided user id", 404)
-      )
-  }
-
-  res.json({place})
-})
+router.get('/user/:uid', placesControllers.getPlaceByUserId)
 
 module.exports = router
