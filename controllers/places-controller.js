e202029b622d238767751cfc45b3c286bfa77117
annastepanova <annastepanova1811@gmail.com>
2020-05-16 12:22:41 -0400

    creating a place and storing in mongoDB

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -3,6 +3,7 @@ const { validationResult } = require('express-validator')
 
 const HttpError = require('../models/http-error')
 const getCoordsForAddress = require('../util/location')
+const Place = require('../models/place')
 
 let DUMMY_PLACES = [
   {
@@ -63,16 +64,22 @@ const createPlace = async (req, res, next) => {
     return next(error)
   }
 
-  const createdPlace = {
-    id: uuid(),
+  const createdPlace = new Place({
     title,
     description,
-    location: coordinates,
     address,
+    location: coordinates,
+    image: 'https://cdn.traveltripper.io/site-assets/192_866_18602/media/2018-08-29-113233/small_boston-common-guide.jpg',
     creator
-  }
+  })
 
-  DUMMY_PLACES.push(createdPlace)
+  try {
+    await createdPlace.save()
+  }
+  catch (err) {
+    const error = new HttpError('Creating place failed, please try again', 500)
+    return next(error)
+  }
 
   res.status(201).json({place: createdPlace})
 
