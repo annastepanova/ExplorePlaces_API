f7e3952a08f3238995cd9eb40c23bdf26065462b
annastepanova <annastepanova1811@gmail.com>
2020-05-28 20:37:28 -0400

    authorization added on update and delete places

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -98,7 +98,7 @@ const login = async (req, res, next) => {
   }
 
   if(!existingUser) {
-    const error = new HttpError('Invalid credentials, could not log you in', 401)
+    const error = new HttpError('Invalid credentials, could not log you in', 403)
     return next(error)
   }
 
@@ -112,7 +112,7 @@ const login = async (req, res, next) => {
   }
 
   if (!isValidPassword) {
-    const error = new HttpError('Invalid credentials, could not log you in', 401)
+    const error = new HttpError('Invalid credentials, could not log you in', 403)
     return next(error)
   }
 
--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -9,14 +9,14 @@ module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1] 
     if (!token) {
-      throw new HttpError('Authentication failed', 401)
+      throw new HttpError('Authentication failed', 403)
     }
     const decodedToken = jwt.verify(token, 'supersecret_dontshare')
     req.userData = { userId: decodedToken.userId}
     next()
   }
   catch (err) {
-    const error = new HttpError('Authentication failed', 401)
+    const error = new HttpError('Authentication failed', 403)
     return next(error)
   }
 }
